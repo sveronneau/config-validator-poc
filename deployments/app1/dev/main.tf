af5623ba56c0bf1ebd4b1b9ff8d434951c0e45ad
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2021-09-14 10:11:28 -0400

    fix

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -2,8 +2,8 @@ resource "google_storage_bucket" "image-store-1" {
   name     = "tj4h-random-test-image-store-bucket-1"
   force_destroy = true
   #
-  location = "europe-north1"
-  #location = "northamerica-northeast2"
+  #location = "europe-north1"
+  location = "northamerica-northeast2"
   #labels   = {
   #    cost-center  = "marketing"
   #    env = "qa"
