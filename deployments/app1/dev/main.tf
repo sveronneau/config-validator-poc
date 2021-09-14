6c732831cb0f71e870538ad3f16f3ffe96f336d2
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2021-09-14 10:26:29 -0400

    bad values

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -2,9 +2,10 @@ resource "google_storage_bucket" "image-store-1" {
   name     = "tj4h-random-test-image-store-bucket-1"
   force_destroy = true
   #
-  #location = "europe-north1"
-  location = "northamerica-northeast2"
+  location = "europe-north1"
+  #location = "northamerica-northeast2"
   #labels   = {
+      cost-center  = "IT"
   #    cost-center  = "marketing"
   #    env = "qa"
   #}
