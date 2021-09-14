75bd10e38e6bf36bdce336f8dd6bd87ffa2cf32f
Stacy Véronneau <stacyv@slalom.com>
2021-09-14 09:48:19 -0400

    fix

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,4 +1,11 @@
 resource "google_storage_bucket" "image-store-1" {
   name     = "tj4h-random-test-image-store-bucket-1"
+  force_destroy = true
+  #
   location = "europe-north1"
+  #location = "northamerica-northeast2"
+  labels   = {
+      cost-center  = "marketing"
+      env = "qa"
+  }
 }
